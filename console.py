747c29ed7dc1d5a47cdfd95967ed9ff079b1f434
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-19 21:56:37 +0100

    MySQL setup development

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -122,7 +122,7 @@ class HBNBCommand(cmd.Cmd):
         elif c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
+
         if not kwargs_string:
             kwargs = {}
         else:
@@ -131,7 +131,8 @@ class HBNBCommand(cmd.Cmd):
                 key, value_str = kv_pair.strip().split('=', 1)
                 try:
                     if value_str[0] == '"' and value_str[-1] == '"':
-                        value = value_str[1:-1].replace('\\"', '"').replace('_', ' ')
+                        value = value_str[1:-1].replace('\\"', '"').\
+                                replace('_', ' ')
                     elif '.' in value_str:
                         value = float(value_str)
                     else:
@@ -206,7 +207,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -338,6 +339,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- prepares a MySQL server for the Airbnb project
+
+-- create a database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+
+-- create a new user and grant permisions
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
