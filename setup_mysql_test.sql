6b82157c5e0449ea08e7e9444f19e7a19947a498
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-19 22:20:07 +0100

    debug

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,5 +9,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
--- If hbnb_dev user already exists, update the password
+-- If hbnb_test user already exists, update the password
 ALTER USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
