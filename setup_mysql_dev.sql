3dcadbb3ac6e4ba94b26c03cf51e979e2b63d334
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-19 22:14:15 +0100

    debug

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,4 +7,7 @@ USE hbnb_dev_db;
 -- create a new user and grant permisions
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- If hbnb_dev user already exists, update the password
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,4 +7,7 @@ USE hbnb_test_db;
 -- create a new user and grant permisions
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- If hbnb_dev user already exists, update the password
+ALTER USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
