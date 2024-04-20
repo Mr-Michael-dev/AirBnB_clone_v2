e35fe358649727c1cb917539d85dfad2cce55d34
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-04-20 10:46:58 +0100

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,11 @@ def hello():
     """displays a text to the client on the default route"""
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays a text to the client on the route /hbnb"""
+    return "HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
